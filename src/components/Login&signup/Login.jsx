2a8446858e34860531b22011e50a11dbdc53c4eb
Raj05052002 <thrajkumar9@gmail.com>
2025-04-17 15:38:35 +0530

    Raj

--- src/components/Login&signup/Login.jsx
+++ src/components/Login&signup/Login.jsx
@@ -104,9 +104,9 @@ const Login = () => {
 
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen bg-[url(../src/assets/login.jpg)] bg-contain">
       <ToastContainer />
-      <div className="relative w-[768px] max-w-full min-h-[480px] bg-white rounded-lg shadow-lg overflow-hidden transition-transform duration-700">
+      <div className="relative w-[768px] max-w-full min-h-[480px] backdrop-blur-xs rounded-lg shadow-white shadow-inner overflow-hidden transition-transform duration-700">
         {/* Forms Container */}
         <div
           className={`absolute top-0 left-0 w-full h-full flex transition-transform duration-700 
@@ -120,31 +120,31 @@ const Login = () => {
           >
             {/* Sign In Form */}
             <div className="w-1/2 h-full flex flex-col items-center justify-center p-10">
-              <h1 className="text-2xl font-bold">Sign In</h1>
+              <h1 className="text-2xl text-gray-50 font-bold">Sign In</h1>
 
-              <span className="text-sm">or use your account</span>
+              <span className="text-sm text-gray-50">or use your account</span>
               <input
                 type="email"
                 name="email"
                 placeholder="Email"
-                className="w-full p-3 my-2 bg-gray-200 rounded"
+                className="w-full p-3 my-2 bg-cyan-100 rounded"
                 onChange={handleform}
               />
               <input
                 type="password"
                 placeholder="Password"
-                className="w-full p-3 my-2 bg-gray-200 rounded"
+                className="w-full p-3 my-2 bg-cyan-100 rounded"
                 name="password"
                 onChange={handleform}
               />
               <a
                 href="#"
-                className="text-blue-500 text-sm"
+                className="text-gray-100 text-sm"
                 onClick={() => setIsForgotPasswordActive(true)}
               >
                 Forgot your password?
               </a>
-              <button className="bg-red-500 text-white px-8 py-3 mt-4 cursor-pointer rounded-full transform: active:scale-[0.95]"
+              <button className="bg-cyan-500 hover:bg-cyan-600 shadow-[0_0.7em_0.8em_-0.5em_rgb(0,0,0)] text-white px-8 py-3 mt-4 cursor-pointer rounded-full transform: active:scale-[0.95]"
                 onClick={handleLogin}>
                 Sign In
               </button>
@@ -152,13 +152,13 @@ const Login = () => {
 
             {/* Sign Up Form */}
             <div className="w-1/2 h-full flex flex-col items-center justify-center p-10">
-              <h1 className="text-2xl font-bold">Sign Up</h1>
-              <span className="text-sm">or use your email for registration</span>
+              <h1 className="text-2xl font-bold text-gray-100">Sign Up</h1>
+              <span className="text-sm text-gray-50">or use your email for registration</span>
               <input
                 name="name"
                 type="text"
                 placeholder="Username"
-                className="w-full p-3 my-2 bg-gray-200 rounded"
+                className="w-full p-3 my-2 bg-cyan-100 rounded"
                 onChange={handleform}
                 required
               />
@@ -166,7 +166,7 @@ const Login = () => {
                 name="email"
                 type="email"
                 placeholder="Email"
-                className="w-full p-3 my-2 bg-gray-200 rounded"
+                className="w-full p-3 my-2 bg-cyan-100 rounded"
                 onChange={handleform}
                 required
               />
@@ -174,12 +174,12 @@ const Login = () => {
                 name="password"
                 type="password"
                 placeholder="Password"
-                className="w-full p-3 my-2 bg-gray-200 rounded"
+                className="w-full p-3 my-2 bg-cyan-100 rounded"
                 onChange={handleform}
                 required
               />
               <button
-                className="bg-red-500 text-white px-8 py-3 mt-4 cursor-pointer rounded-full transform: active:scale-[0.95]"
+                className="bg-cyan-500 hover:bg-cyan-600 text-white px-8 py-3 mt-4 cursor-pointer rounded-full shadow-[0_0.7em_0.8em_-0.5em_rgb(0,0,0)] transform: active:scale-[0.95]"
                 onClick={handlesignin}
               >
                 Sign Up
@@ -190,22 +190,22 @@ const Login = () => {
 
         {/* Forgot Password Form */}
         <div
-          className={`absolute top-full left-0 w-full h-full flex flex-col items-center justify-center p-10 bg-white transition-transform duration-700 ${isForgotPasswordActive ? "-translate-y-full" : "translate-y-0"
+          className={`absolute top-full left-0 w-full h-full flex flex-col items-center justify-center p-10 bg-transparent transition-transform duration-700 ${isForgotPasswordActive ? "-translate-y-full" : "translate-y-0"
             }`}
         >
-          <h1 className="text-2xl font-bold">Reset Password</h1>
-          <span className="text-sm">Enter your email to reset password</span>
+          <h1 className="text-2xl font-bold text-gray-700">Reset Password</h1>
+          <span className="text-sm text-gray-700">Enter your email to reset password</span>
           <input
             type="email"
             placeholder="Email"
             className="w-[80%] p-3 my-2 bg-gray-200 rounded"
           />
-          <button className="bg-red-500 text-white px-8 py-3 mt-4 rounded-full">
+          <button className="bg-cyan-600 shadow-[0_0.7em_0.8em_-0.5em_rgb(0,0,0)] text-white px-8 py-3 mt-4 rounded-full cursor-pointer hover:bg-cyan-700">
             Reset Password
           </button>
           <a
             href="#"
-            className="text-blue-500 text-sm mt-4"
+            className="text-cyan-300 text-sm mt-4"
             onClick={() => setIsForgotPasswordActive(false)}
           >
             Back to Sign In
@@ -214,7 +214,7 @@ const Login = () => {
 
         {/* OTP Form */}
         <div
-          className={`absolute bottom-0 left-0 w-full h-full flex flex-col items-center justify-center p-10 bg-white transition-transform duration-700 ${isOtpActive ? "translate-y-0" : "-translate-y-100"
+          className={`absolute bottom-0 left-0 w-full h-full flex flex-col items-center justify-center p-10 bg-transparent transition-transform duration-700 ${isOtpActive ? "translate-y-0" : "-translate-y-100"
             }`}
         >
           <h1 className="text-2xl font-bold">Otp</h1>
@@ -225,14 +225,14 @@ const Login = () => {
             placeholder="Enter Otp"
             className="w-[80%] p-3 my-2 bg-gray-200 rounded"
           />
-          <button className="bg-red-500 text-white px-8 py-3 mt-4 rounded-full"
+          <button className="bg-cyan-600 hover:bg-cyan-700 text-white shadow-[0_0.7em_0.8em_-0.5em_rgb(0,0,0)] px-8 py-3 mt-4 rounded-full"
             onClick={handleOtpSubmit}
           >
             Submit
           </button>
           <a
             href="#"
-            className="text-blue-500 text-sm mt-4"
+            className="text-cyan-300 text-sm mt-4"
             onClick={() => setIsOtpActive(false)}
           >
             Back to Sign Up
@@ -242,7 +242,7 @@ const Login = () => {
         {/* Overlay */}
         {!isForgotPasswordActive && !isOtpActive && (
           <div
-            className={`absolute left-1/2 w-1/2 h-full bg-gradient-to-r from-red-500 to-pink-500 text-white flex flex-col items-center justify-center p-10 transition-all duration-700 
+            className={`absolute left-1/2 w-1/2 h-full bg-[url(../src/assets/bglogin.png)] bg-cover bg-no-repeat text-black flex flex-col items-center justify-center p-10 transition-all duration-700 
             ${isRightPanelActive ? "-translate-x-full" : "translate-x-0"}
             `}
           >
@@ -253,7 +253,7 @@ const Login = () => {
                   To keep connected with us please login with your personal info
                 </p>
                 <button
-                  className="border border-white text-white px-8 cursor-pointer py-3 mt-4 rounded-full"
+                  className="border border-gray-400 text-gray-800 bg-[#95f3e2] shadow-[0_0.7em_0.8em_-0.5em_rgb(0,0,0)] px-8 cursor-pointer py-3 mt-4 rounded-full"
                   onClick={() => setIsRightPanelActive(false)}
                 >
                   Sign In
@@ -266,7 +266,7 @@ const Login = () => {
                   Enter your personal details and start your journey with us
                 </p>
                 <button
-                  className="border border-white text-white px-8 cursor-pointer py-3 mt-4 rounded-full"
+                  className="border border-gray-400 text-gray-800 bg-[#95f3e2] shadow-[0_0.7em_0.8em_-0.5em_rgb(0,0,0)] px-8 cursor-pointer py-3 mt-4 rounded-full"
                   onClick={() => setIsRightPanelActive(true)}
                 >
                   Sign Up
