8069092ffdf9af5ae512a8b114a6c5ffab1c06da
rahulthakur2001 <rahulthakur95609@gmail.com>
2025-07-20 05:13:44 +0530

    Donem

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{
-    host:true
+  plugins: [react()],
+  server: {
+    host: true,
   },
   build: {
     rollupOptions: {
-      external: ['some-external-package'],
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Put all node_modules into a vendor chunk
+            return 'vendor';
+          }
+          // You can add more conditions for other manual chunks if needed
+        },
+      },
     },
+    chunkSizeWarningLimit: 1500, // optional: increase limit if you want to suppress warnings below this size
   },
 })
